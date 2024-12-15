cb2d8e8c5dd007a9f7bb3e13d3290204b06039fb
remya-1993 <remyanivedhya9@gmail.com>
2024-12-15 21:27:50 +0530

    design completed

---
+++ kd.js
@@ -0,0 +1,18 @@
+function toggleMenu() {
+  var x = document.getElementById("myTopnav");
+  x.classList.toggle("responsive");
+}
+
+function more(){
+  window.location.assign("ear.html")
+}
+
+function more1(){
+  window.location.assign("neck.html")
+}
+function more2(){
+  window.location.assign("bangles.html")
+}
+function more3(){
+  window.location.assign("rings.html")
+}
---
+++ neck.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+  var x = document.getElementById("myTopnav");
+  x.classList.toggle("responsive");
+}
+
+function imgdetails1() {
+  window.location.assign("neckpieces.html")
+} 
