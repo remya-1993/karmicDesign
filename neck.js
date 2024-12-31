665e4214ffe17fc0fa5cd3139e602bed89ccedbe
remya-1993 <remyanivedhya9@gmail.com>
2024-12-31 20:25:31 +0530

    almostdone

--- neck.js
+++ neck.js
@@ -6,3 +6,40 @@ function toggleMenu() {
 function imgdetails1() {
   window.location.assign("neckpieces.html")
 } 
+
+function buynow1(){
+  window.location.assign("necksub.html")
+}
+
+function showModal(title, price, imgSrc, description) {
+
+  document.getElementById('modal-title').textContent = title;
+  document.getElementById('modal-price').textContent = price;
+  document.getElementById('modal-img').src = imgSrc;
+  document.getElementById('modal-description').textContent = description;
+
+  document.getElementById('popup1').style.display = 'flex';
+}
+
+function closeModal() {
+  document.getElementById('popup1').style.display = 'none';
+}
+
+function showModal1(cusName, cusNumber, quantity){
+  document.getElementById('cusName').value = cusName;
+  document.getElementById('cusNumber').value = cusNumber;
+  document.getElementById('quantity').value= quantity;
+
+  document.getElementById('popup1').style.display = 'none'; 
+  document.getElementById('popup2').style.display = 'block'; 
+}
+
+function closeModal() {
+  // Close both popups
+  document.getElementById('popup1').style.display = 'none';
+  document.getElementById('popup2').style.display = 'none';
+}
+
+function showPopup2() {
+  document.getElementById('popup2').style.display = 'flex';
+}
