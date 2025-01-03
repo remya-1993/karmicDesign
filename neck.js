03a8f77c5058f86dab8d6a3ebc8eca1697e789c9
remya-1993 <remyanivedhya9@gmail.com>
2025-01-03 10:43:18 +0530

    popup media query

--- neck.js
+++ neck.js
@@ -179,10 +179,6 @@ function whatsapp() {
     alert("Please fill out all fields before sending the message.");
     return;
   }
-  // if (isNaN(phone) || phone.length < 10 || phone.length > 10) {
-  //   alert("Please enter a valid 10-digit phone number.");
-  //   return;
-  // }
 
   var phonenumber = "918590210665";
   var url = "https://wa.me/" + phonenumber + "?text=" +
